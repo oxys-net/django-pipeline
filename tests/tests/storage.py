1ec98190ee420d3aa31f5e68f2283e128179d9f0
David Charbonnier <d.charbonnier@oxys.net>
2012-04-30 17:05:04 +0200

    clean old code

--- tests/tests/storage.py
+++ tests/tests/storage.py
@@ -40,28 +40,3 @@ class StorageTest(TestCase):
         settings.PIPELINE_COMPILERS = self.old_compilers
        
     
-    
-"implement exists, path, list, listdir"    
-"""
-class StorageTest(TestCase):
-    def setUp(self):
-        settings.PIPELINE_CSS = {
-            'testing': {
-                'source_filenames': (
-                    'css/first.css',
-                ),
-                'manifest': False,
-                'output_filename': 'testing.css',
-            }
-        }
-        settings.PIPELINE_JS_COMPRESSOR = None
-        settings.PIPELINE_CSS_COMPRESSOR = None
-        self.storage = PipelineStorage()
-
-    def test_listdir(self):
-        files = self.storage.listdir()
-        self.assertEqual(files, ([],['testing.css', 'scripts.css']))
-
-    def tearDown(self):
-        settings.PIPELINE_CSS = {}
-"""
